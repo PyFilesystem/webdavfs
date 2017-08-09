c092b8c9473651cc00ea894d3768c79ac13ec80f
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-08-10 01:31:07 +0200

    Amend webdavfs.tests to use TestCase.setUp and contexts

--- webdavfs/tests/test_webdavfs.py
+++ webdavfs/tests/test_webdavfs.py
@@ -12,29 +12,26 @@ from fs.test import FSTestCases
 
 from nose.plugins.attrib import attr
 
-webdav_url = os.environ.get('FS_WEBDAV_URL', 'http://admin:admin@zopyx.com:20082/exist/webdav/db')
+webdav_url = os.environ.get(
+    'FS_WEBDAV_URL',
+    'http://admin:admin@zopyx.com:20082/exist/webdav/db'
+).replace('http', 'webdav')
+
 
 @attr('slow')
 class TestWebDAVFS(FSTestCases, unittest.TestCase):
     """Test WebDAVFS implementation."""
 
-
-    def __init__(self, *args, **kw):
+    def setUp(self):
         self.test_root = unicode(uuid.uuid4())
-        super(TestWebDAVFS, self).__init__(*args, **kw)
+        super(TestWebDAVFS, self).setUp()
 
     def make_fs(self):
-
-        f = furl.furl(webdav_url)
-        url = '{0}://{1}:{2}'.format(f.scheme, f.host, f.port)
-        creds = {'login': f.username, 'password': f.password}
-        root = str(f.path)
-        handle = webdavfs.WebDAVFS(url, creds, root)
-        if handle.exists(self.test_root):
-            handle.rmdir(self.test_root, recursive=True)
-        handle.makedir(self.test_root)
-        handle = webdavfs.WebDAVFS(url, creds, root + '/' + self.test_root)
-        return fs.open_fs(webdav_url.replace('http', 'webdav'))
+        with fs.open_fs(webdav_url) as handle:
+            if handle.exists(self.test_root):
+                handle.rmdir(self.test_root, recursive=True)
+            handle.makedir(self.test_root)
+        return fs.open_fs('/'.join([webdav_url, self.test_root]))
 
     def destroy_fs(self, fs):
         for item in fs.client.list('/'):
