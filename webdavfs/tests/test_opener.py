83b85aff7d61e9e1eca7988c8dc0f8655439738f
Andreas Jung <info@zopyx.com>
2019-04-29 14:36:15 -0400

    added

---
+++ webdavfs/tests/test_opener.py
@@ -0,0 +1,10 @@
+from fs.opener import open_fs
+
+
+def test_opener_webdav():
+    result = open_fs('webdav://foo.bar/webdav')
+    assert result.url.startswith('http://foo.bar')
+
+def test_opener_webdav_443():
+    result = open_fs('webdav://foo.bar:443/webdav')
+    assert result.url.startswith('https://foo.bar')
